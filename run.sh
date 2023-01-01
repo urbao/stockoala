c78058b1d09652f09a2eb3a22cab6822ad2204dc
urbao <E24094198@gs.ncku.edu.tw>
2023-01-02 07:44:26 +0800

    create run.sh

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# this shell script will be the main part of 'stockoala'
+# It controls all user behavior, and decide to run which python script 
+# created by https://github.com/urabo for project 'stockoala'
+# further info: https://github.com/urbao/stockoala
+
+#--------#--Basic Definition------------#
+
+
+#---------------------------------------#
+
+
+#------------Side Functions-------------#
+
+
+#---------------------------------------#
+
+
+#------------Main Functions-------------#
+
+
+#---------------------------------------#
