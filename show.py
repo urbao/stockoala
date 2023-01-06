5aff8d2c6fe00c9aac81e64299e5ac58133e5dd7
urbao <E24094198@gs.ncku.edu.tw>
2023-01-06 13:47:46 +0800

    update some idea

---
+++ show.py
@@ -0,0 +1,5 @@
+from ouptut import color_output
+
+# this script used to show datafiles content
+# used ref: output.py for showing result
+# used ref: get.py for getting user's input of col_size && open_filename
