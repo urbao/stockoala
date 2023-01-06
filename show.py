01173dfc2c0d040fc644422760fe5d25895f1c7c
urbao <E24094198@gs.ncku.edu.tw>
2023-01-06 16:35:13 +0800

    temp
    temp before merge

--- show.py
+++ show.py
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 from ouptut import color_output
 
 # this script used to show datafiles content
 # used ref: output.py for showing result
 # used ref: get.py for getting user's input of col_size && open_filename
+=======
+from output import color_output
+
+>>>>>>> 6bc09c9 (temp)
