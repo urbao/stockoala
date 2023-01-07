d63807a25a6326fe5729a5f16f2f7d1578757825
Eason Chen <87600155+urbao@users.noreply.github.com>
2023-01-07 13:04:39 +0800

    Update collect.py

--- collect.py
+++ collect.py
@@ -38,4 +38,3 @@ output.color_output("cyan", "==============$$$$===============", True)
 import os
 os.rename(start_date+"[total].txt", start_date+".txt") # rename for simplicity
 output.color_output("white", "For more data details, use viewer to check the data files\n", True)
-os.chmod(start_date+".txt", 0o444)
