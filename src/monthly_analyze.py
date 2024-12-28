3739cbda9345bf1cb1039959053e948633e4f594
Eason Chen <87600155+urbao@users.noreply.github.com>
2024-12-28 21:33:13 +0800

    Update monthly_analyze.py
    - Add High/Open/Close price into monthly data when updating monthly data
    - Add an extra rule for checking monthly data

--- src/monthly_analyze.py
+++ src/monthly_analyze.py
@@ -7,36 +7,79 @@ import os
 import json
 
 
-def update_lowest_low(low_price_dict, id_val, low_val):
+def update_lowest_low(monthly_price_dict, id_val, low_val):
     # consider the case that low_val is '----'
     if low_val=='--' or low_val=='----':
         return
 
-    # if id_val existed in dic, then choose the lowest low_val
-    if id_val in low_price_dict:
-        low_price_dict[id_val] = min(low_price_dict[id_val], float(low_val))
-    # otherwise, append it to dict
+    # if id_val existed in dict, then choose the lowest low_val
+    if id_val in monthly_price_dict:
+        monthly_price_dict[id_val]['low'] = min(monthly_price_dict[id_val]['low'], float(low_val))
+    # otherwise, append it to dict and init other high/open/close price at the same time 
+    # (because the update_lowest_low is called at first, no need to do this at the rest of update monthly price functions)
     else:
-        low_price_dict[id_val] = float(low_val)
+        monthly_price_dict[id_val] = {'low': float(low_val), 'high': None, 'open': None, 'close': None}
+
+def update_highest_high(monthly_price_dict, id_val, high_val):
+    if high_val=='--' or high_val=='----':
+        return
+    
+    # if id_val existed in dict, then choose the highest high_val
+    if id_val in monthly_price_dict and monthly_price_dict[id_val]['high'] != None:
+        monthly_price_dict[id_val]['high'] = max(monthly_price_dict[id_val]['high'], float(high_val));
+    # otherwise, append it to dict and init other high/open/close price at the same time
+    else:
+        monthly_price_dict[id_val]['high'] = float(high_val)
+
+def update_monthly_open(monthly_price_dict, id_val, open_val):
+    if open_val=='--' or open_val == '----':
+        return
+    
+    # check if the monthly_price_dict open for that specific id is still null
+    if id_val in monthly_price_dict and monthly_price_dict[id_val]['open'] == None:
+        monthly_price_dict[id_val]['open'] = open_val
+    # if the open is already not null, meaning the open data is no needed to be updated
+    else:
+        return
+    
+def update_monthly_close(monthly_price_dict, id_val, close_val):
+    if close_val=='--' or close_val=='----':
+        return
+
+    # no matter what, just keep update the close_val
+    # reason: the file is loaded ascending in date, so if we keep update
+    # , and we'll get the last day close_val
+    if id_val in monthly_price_dict:
+        monthly_price_dict[id_val]['close'] = close_val
+        return
 
 # update specific month's monthly report
 def update_specific_month_report(path, monthly_report):
     # first save all the files content into an dict
-    low_price_dict={}
-    for filename in os.listdir(str(path)):
+    monthly_price_dict={}
+    # we sort the filename in Ascending mode, so we can know the open and close price of that month
+    for filename in sorted(os.listdir(str(path))):
         filepath=os.path.join(path, filename)
         if os.path.isfile(filepath) and filename.endswith('.txt'):
             with open(filepath, 'r') as file:
                 for line in file:
                     data = line.replace('\'','').replace('[','').replace(']','').split(',')
                     id_val = data[0].strip()  # Assuming 'Id' is the first value
+                    high_val = data[1].strip()
                     low_val = data[2].strip()  # Assuming 'low' is the third value
-                    update_lowest_low(low_price_dict, id_val, low_val)
+                    open_val = data[3].strip()
+                    close_val = data[4].strip()
+                    
+                    # calling the function to update the monthly price data
+                    update_lowest_low(monthly_price_dict, id_val, low_val)
+                    update_highest_high(monthly_price_dict, id_val, high_val)
+                    update_monthly_open(monthly_price_dict, id_val, open_val)
+                    update_monthly_close(monthly_price_dict, id_val, close_val)
 
     # save the updated month report(Id and low_val) to a file
     # format: json, get the low_val more quickly
     with open(os.path.join(path, monthly_report+".json"), 'w') as json_file:
-        json.dump(low_price_dict, json_file)
+        json.dump(monthly_price_dict, json_file)
     return
         
 
@@ -83,18 +126,30 @@ def monthly_strategy_analysis(path_list, weekly_analysis_result):
     Monthly-Strategy (2 conditions)
         - case 1: this_month low_val < one_month_ago low_val
         - case 2: one_month_ago low_val < two_month_ago low_val
+        - case 3: this_month low_val(high_val) > one_month_ago low_val(high_val)
+                                                +
+                  one_month_ago low_val(high_val) > two_month_ago low_val(high_val)
+                                                +
+                  this_month open > this_month close
     '''
 
     for stockid in weekly_analysis_result:
         # case 1
         if stockid in this_month and stockid in one_month_ago:
-            if this_month[stockid]<one_month_ago[stockid]:
+            if this_month[stockid]['low']<one_month_ago[stockid]['low']:
                 final_result.append(stockid)
                 continue
         # case 2
         if stockid in one_month_ago and stockid in two_month_ago:
-            if one_month_ago[stockid]<two_month_ago[stockid]:
+            if one_month_ago[stockid]['low']<two_month_ago[stockid]['low']:
                 final_result.append(stockid)
                 continue
+        # case 3
+        if stockid in one_month_ago and stockid in two_month_ago:
+            if this_month[stockid]['low'] > one_month_ago[stockid]['low'] and this_month[stockid]['high'] > one_month_ago[stockid]['high']:
+                if one_month_ago[stockid]['low'] > two_month_ago[stockid]['low'] and one_month_ago[stockid]['high'] > two_month_ago[stockid]['high']:
+                    if this_month[stockid]['open'] > this_month[stockid]['close']:
+                        final_result.append(stockid)
+                        continue
     return final_result
     
